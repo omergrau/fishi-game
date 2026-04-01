129543718572d270d22a27ec241fb4e09f5e4cbd
omergrau <omergrau@gmail.com>
2026-04-01 19:27:50 +0300

    adding the puctionalities of the shield itselfs

--- extras/Player.py
+++ extras/Player.py
@@ -1,11 +1,12 @@
 import pygame
+
 from extras import resources
 
 
 class player(pygame.sprite.Sprite):
-    def __init__(self, WIDTH, HEIGHT,crunch,left_image=0,right_image=0):
+    def __init__(self, WIDTH, HEIGHT, crunch, left_image=0, right_image=0):
         super().__init__()
-        self.picturs = [("../assets/images/my fish left.png","../assets/images/my fish right.png")]
+        self.picturs = [("../assets/images/my fish left.png", "../assets/images/my fish right.png")]
         self.x = WIDTH // 2
         self.y = HEIGHT // 2
         self.score = 0
@@ -21,12 +22,30 @@ class player(pygame.sprite.Sprite):
         self.image = self.images[1]
         self.mask = pygame.mask.from_surface(self.image)
         self.eating_music = pygame.mixer.Sound(crunch)
-        self.shield = False
+        self.shield = None
+        self.shield_cooldown_ms = 2000
+        self.shield_cooldown_start = None
+
+    def cooldown_image(self):
+        if self.shield_cooldown_start is not None:
+            tint_color = (120, 120, 120, 0)
+            self.image = pygame.transform.scale(self.image, (self.size, self.size))
+            colored_image = self.image.copy()
+            tint_surface = pygame.Surface(colored_image.get_size(), pygame.SRCALPHA)
+            tint_surface.fill(tint_color)
+            self.rect = self.image.get_rect()
+            self.rect.topleft = (self.x, self.y)
+            self.image.set_colorkey((255, 255, 255))
+            colored_image.blit(tint_surface, (0, 0), special_flags=pygame.BLEND_RGB_ADD)
+            self.image = colored_image
+
 
     def update(self, dt):  # dt in seconds
         slowdown = self.slowdownspeed * dt * 20
         idle_time = (pygame.time.get_ticks() - self.lastmove) / 1000
-
+        if self.shield_cooldown_start is not None and pygame.time.get_ticks() - self.shield_cooldown_start > self.shield_cooldown_ms:
+            self.shield_cooldown_start = None
+            self.shield = None
         if self.accelartiony > 0:
             self.accelartiony -= slowdown
             if self.accelartiony < 0:
@@ -57,8 +76,14 @@ class player(pygame.sprite.Sprite):
                              int(self.y + self.accelartiony * dt * 60))
         self.mask = pygame.mask.from_surface(self.image)
         self.check_borders()
+        if self.shield:
+            self.shield.update(self.rect.x, self.rect.y,self.size)
+        if self.shield_cooldown_start is not None and not self.shield:
+            self.cooldown_image()
 
-    def eating(self,size):# Feeding the fish - increase the player size and score
+
+
+    def eating(self, size):  # Feeding the fish - increase the player size and score
         self.score += 1
         self.size += size // 20
         self.image = pygame.transform.scale(self.image, (self.size, self.size))
@@ -67,11 +92,11 @@ class player(pygame.sprite.Sprite):
         self.eating_music.play()
         pygame.mixer.music.set_volume(0.5)
 
-    def level_up(self):# level up the player - increase the player size and score)(not in use)
+    def level_up(self):  # level up the player - increase the player size and score)(not in use)
         self.level += 1
-        self.score=0
+        self.score = 0
         if len(self.picturs) == self.level:
-            self.level-=1
+            self.level -= 1
         image_right = pygame.image.load(resources.load_resource(self.picturs[self.level][1])).convert_alpha()
         self.rect = image_right.get_rect()
         image_right = pygame.transform.scale(image_right, (self.size, self.size))
@@ -81,43 +106,7 @@ class player(pygame.sprite.Sprite):
         image_left = pygame.transform.scale(image_left, (self.size, self.size))
         self.mask = pygame.mask.from_surface(image_left)
         self.images = (image_left, image_right)
-        self.image= self.images[1]
-
-    """def move(self, keys):# move the player based on the keys pressed
-        if keys[pygame.K_LEFT]:
-            self.lastmove = pygame.time.get_ticks()
-            if abs(self.accelartionx) < 5:
-
-                if pygame.time.get_ticks() - self.dellay > 15:
-                    self.dellay = pygame.time.get_ticks()
-                    self.accelartionx -= self.speed
-            self.x += self.speed
-            self.image = pygame.transform.scale(self.images[0], (self.size, self.size))
-            self.mask = pygame.mask.from_surface(self.image)
-        if keys[pygame.K_RIGHT]:
-            self.lastmove = pygame.time.get_ticks()
-            self.image = self.images[1]
-            if abs(self.accelartionx) < 5:
-                if pygame.time.get_ticks() - self.dellay > 15:
-                    self.dellay = pygame.time.get_ticks()
-                    self.accelartionx += self.speed
-            self.x += self.speed
-            self.image = pygame.transform.scale(self.images[1], (self.size, self.size))
-            self.mask = pygame.mask.from_surface(self.image)
-        if keys[pygame.K_UP]:
-            self.lastmove = pygame.time.get_ticks()
-            if abs(self.accelartiony) < 5:
-                if pygame.time.get_ticks() - self.dellay > 15:
-                    self.dellay = pygame.time.get_ticks()
-                    self.accelartiony -= self.speed
-            self.y -= self.speed
-        if keys[pygame.K_DOWN]:
-            self.lastmove = pygame.time.get_ticks()
-            if abs(self.accelartiony) < 5:
-                if pygame.time.get_ticks() - self.dellay > 15:
-                    self.dellay = pygame.time.get_ticks()
-                    self.accelartiony += self.speed
-            self.y += self.speed"""
+        self.image = self.images[1]
 
     def move(self, keys, dt):
         accel_step = self.speed * dt * 60
@@ -146,7 +135,8 @@ class player(pygame.sprite.Sprite):
             if abs(self.accelartiony) < 5:
                 self.accelartiony += accel_step
 
-    def check_borders(self):# check if the player is out of the game borders and set the player location to the other side of the screen
+    def check_borders(
+            self):  # check if the player is out of the game borders and set the player location to the other side of the screen
         if self.x < 0:
             self.x = 982
         if self.x > 982:
@@ -155,6 +145,3 @@ class player(pygame.sprite.Sprite):
             self.y = 0
         if self.y > 736:
             self.y = 736
-
-
-
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from extras import Player, Fish
 from extras.Player import player
 from extras.constants import *
 from extras.resources import *
+from specialities.ShieldBubble import ShieldBubble
 
 
 class game():
@@ -26,8 +27,10 @@ class game():
         self.lose_sound_playing = False
         self.font = pygame.font.Font(None, 36)
         self.text_color = (255, 255, 255)
+        self.shield_bubble = None
+        self.last_bubble_spawn = pygame.time.get_ticks()
 
-    def setup(self):# setup the game screen and load the game music
+    def setup(self):  # setup the game screen and load the game music
         pygame.mixer.music.load(self.sounds["game music"])
         pygame.mixer.music.set_volume(0.5)
         pygame.mixer.music.play(-1)
@@ -40,20 +43,31 @@ class game():
         for _ in range(10):
             self.fishlist.add(Fish.fish(WIDTH, HEIGHT))
 
-
-    def toggle_fullscreen(self):# toggle the fullscreen from regular to fullscreen and vice versa
+    def toggle_fullscreen(self):  # toggle the fullscreen from regular to fullscreen and vice versa
         if self.fullscreen:
             self.screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
         else:
             self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-    def update_player(self, dt):# update the player location and check for collision with the enemies
+    def update_player(self, dt):  # update the player location and check for collision with the enemies
         keys = pygame.key.get_pressed()
         player1 = self.players.sprites()[0]
-        player1.move(keys,dt)
+        player1.move(keys, dt)
         player1.update(dt)
 
-    def update_enemies(self, dt):# update the enemies location and check for collision with the player
+    def update_shield_bubble(self):
+        now = pygame.time.get_ticks()
+        player1 = self.players.sprites()[0]
+        if self.shield_bubble:
+            if pygame.sprite.collide_mask(player1, self.shield_bubble):
+                player1.shield = self.shield_bubble
+                self.shield_bubble = None
+        if self.shield_bubble is None and not player1.shield:
+            if now - self.last_bubble_spawn > 0:
+                self.shield_bubble = ShieldBubble(WIDTH, HEIGHT,player1.size)
+                self.last_bubble_spawn = now
+
+    def update_enemies(self, dt):  # update the enemies location and check for collision with the player
         for fish_i in self.fishlist:
             if fish_i.isdisappear():
                 self.fishlist.remove(fish_i)
@@ -69,20 +83,28 @@ class game():
                 else:
                     if player1.shield:
                         player1.shield = False
-                    else:
+                        player1.shield_cooldown_start = pygame.time.get_ticks()
+                    elif player1.shield_cooldown_start is None:
                         self.game_mode = "game over"
 
-    def draw(self):# draw the game screen and the player and enemies
+    def draw_shield_Bubble(self):
+        if self.shield_bubble is not None:
+            self.screen.blit(self.shield_bubble.image, self.shield_bubble.rect)
+        elif self.players.sprites()[0].shield:
+            self.screen.blit(self.players.sprites()[0].shield.image, self.players.sprites()[0].shield.rect)
+
+    def draw(self):  # draw the game screen and the player and enemies
         self.fishlist.draw(self.screen)
+        self.draw_shield_Bubble()
         self.players.draw(self.screen)
         self.draw_score()
 
-    def pausef(self, event):# pause the game
+    def pausef(self, event):  # pause the game
         if not self.pause_pressed:
             self.pause = not self.pause
             self.pause_pressed = True
 
-    def update_keyboard_input(self):# check for keyboard input and update the game state
+    def update_keyboard_input(self):  # check for keyboard input and update the game state
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.game_mode = "quit"
@@ -101,30 +123,29 @@ class game():
             else:
                 self.pause_pressed = False
 
-    def load_high_score(self):# load the high score from the file
+    def load_high_score(self):  # load the high score from the file
         highscore = load_resource("highscore.txt")
         if os.path.exists(highscore):
             with open(highscore, "r") as f:
                 try:
-                    self.high_score = max(int(f.readline()),self.players.sprites()[0].score)
+                    self.high_score = max(int(f.readline()), self.players.sprites()[0].score)
                 except ValueError:
-                    self.high_score= 0
+                    self.high_score = 0
         else:
             self.high_score = 0
 
-    def save_high_score(self):# save the high score to the file
+    def save_high_score(self):  # save the high score to the file
         with open(load_resource("highscore.txt"), "w") as f:
             f.write(str(self.high_score))
 
-    def draw_score(self):# draw the score on the screen
+    def draw_score(self):  # draw the score on the screen
         score_text = self.font.render(f"score: {self.players.sprites()[0].score}", True, self.text_color)
         score_rect = score_text.get_rect()
         score_rect.topleft = ((WIDTH - score_rect.width) // 2, 10)
         self.screen.blit(score_text, score_rect)
 
-    def update_game(self):# update the game state and draw the background
+    def update_game(self):  # update the game state and draw the background
         self.screen.blit(self.background, (0, 0))
-        #self.screen.blit(self.images["ocean"].convert(), (0, 0))
         if self.game_mode == "quit":
             self.running = False
         if self.game_mode == "game over":
@@ -139,19 +160,20 @@ class game():
             score_rect = text.get_rect(center=((WIDTH // 2), (HEIGHT // 2) + 80))
             self.screen.blit(text, score_rect)
 
-    def update(self,dt):# update the game state and draw the background
+    def update(self, dt):  # update the game state and draw the background
         self.update_player(dt)
         self.update_enemies(dt)
         self.update_keyboard_input()
+        self.update_shield_bubble()
         self.update_game()
 
-    def game_over_sound(self):# play the game over sound
+    def game_over_sound(self):  # play the game over sound
         pygame.mixer.music.load(self.sounds["lose"])
         pygame.mixer.music.set_volume(0.5)
         pygame.mixer.music.play()
         self.lose_sound_playing = True
 
-    async def game(self):## main game loop
+    async def game(self):  ## main game loop
         self.running = True
         self.setup()
         while self.running:
@@ -174,26 +196,26 @@ class game():
             await asyncio.sleep(0)
         pygame.quit()
 
-    async def game_over(self):## game over screen and restart option for the game
+    async def game_over(self):  ## game over screen and restart option for the game
         if not self.lose_sound_playing:
             self.game_over_sound()
         if self.game_mode == "restart":
             self.lose_sound_playing = False
-            self.game_mode="game"
+            self.game_mode = "game"
             self.__init__()
             await self.game()
         await asyncio.sleep(0)
 
-    def load_game_resources(self):## load the game resources
+    def load_game_resources(self):  ## load the game resources
         self.load_images()
         self.load_sounds()
 
-    def load_images(self):## load the game images
+    def load_images(self):  ## load the game images
         self.images["my fish left"] = load_resource("../assets/images/my fish left.png")
         self.images["my fish right"] = load_resource("../assets/images/my fish right.png")
         self.images["ocean"] = load_resource("../assets/images/ocean.png")
 
-    def load_sounds(self):## load the game sounds
+    def load_sounds(self):  ## load the game sounds
         self.sounds["game music"] = load_resource("../assets/music/game-music-loop.wav")
         self.sounds["lose"] = load_resource("../assets/music/lose_video-game.wav")
         self.sounds["eat"] = load_resource("../assets/music/plastic-crunch.wav")
